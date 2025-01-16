b0f74490a74a37384a2f2b7843ed2f3d1dd0df24
tlouro-c <tomascorreia.lc@gmail.com>
2025-01-16 22:51:03 +0000

    Fix WPP button

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -22,7 +22,7 @@ export const Footer = () => {
                         target="_blank"
                         rel="nopener nofollow"
                         className={
-                            "flex items-center gap-4 p-2 bg-[#25D366]/10 text-[#25D366] rounded-md hover:bg-[#25D366]/20 transition-colors"
+                            "flex items-center gap-4 px-4 py-2 bg-[#25D366]/10 text-[#25D366] rounded-md hover:bg-[#25D366]/20 transition-colors w-fit"
                         }
                     >
                         <span>Fala Connosco</span>
