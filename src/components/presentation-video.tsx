49adea9a53aea59aad1ffdc7b3182cdb34bb4535
tlouro-c <tomascorreia.lc@gmail.com>
2025-01-16 14:38:23 +0000

    Fix video

--- src/components/presentation-video.tsx
+++ src/components/presentation-video.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 export const PresentationVideo = () => {
+
     return (
         <div className="aspect-video rounded-lg overflow-hidden shadow-lg">
             <iframe
-                src="https://www.youtube.com/embed/NgymAPexlt0?si=pBB7hSwp9Cu58iRd&rel=0&showinfo=0&enablejsapi=1"
+                src={`https://www.youtube-nocookie.com/embed/NgymAPexlt0?&si=pBB7hSwp9Cu58iRd&rel=0&showinfo=0`}
                 title="Vídeo de apresentação Team Lino"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
+                sandbox="allow-scripts allow-same-origin allow-presentation allow-popups"
                 allowFullScreen
                 className={"w-full aspect-video"}>
             </iframe>
