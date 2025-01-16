0e6c3d7672eef0837570923289fbfe403b6ea97f
tlouro-c <tomascorreia.lc@gmail.com>
2025-01-16 14:24:12 +0000

    Video title

--- src/components/presentation-video.tsx
+++ src/components/presentation-video.tsx
@@ -5,9 +5,10 @@ export const PresentationVideo = () => {
         <div className="aspect-video rounded-lg overflow-hidden shadow-lg">
             <iframe
                 src="https://www.youtube.com/embed/NgymAPexlt0?si=pBB7hSwp9Cu58iRd&rel=0&showinfo=0&enablejsapi=1"
-                title="YouTube video player"
+                title="Vídeo de apresentação Team Lino"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
                 className={"w-full aspect-video"}>
             </iframe>
         </div>
