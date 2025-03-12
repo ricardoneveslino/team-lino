01db0b69e82fb1b3e8f8d6e3ab25cdc658be435e
tlouro-c <tomascorreia.lc@gmail.com>
2025-03-12 14:44:30 +0000

    Fix

--- src/features/hero/hero.tsx
+++ src/features/hero/hero.tsx
@@ -8,9 +8,9 @@ export function Hero() {
     return (
         <div className={"h-full w-full flex flex-col items-center md:justify-center gap-12 sm:gap-32"}>
 
-            <div className={"flex flex-col gap-16 sm:flex-row items-center justify-center w-full py-8 md:py-0"}>
+            <div className={"flex flex-col gap-16 sm:flex-row items-center justify-center w-full"}>
 
-                <div className={"w-full sm:w-6/12 flex flex-col gap-6 items-center sm:items-start"}>
+                <div className={"w-full sm:w-6/12 flex flex-col gap-4 items-center sm:items-start"}>
 
                     <h1 className="text-center text-3xl font-bold md:text-4xl lg:text-6xl xl:text-7xl text-pretty sm:text-left">
                         Transforma Objetivos em{" "}
@@ -22,12 +22,12 @@ export function Hero() {
 
                     <PresentationVideo className={"sm:hidden"}/>
 
-                    <div className={"flex flex-col gap-6 text-center sm:text-left"}>
+                    <div className={"flex flex-col gap-4 text-center sm:text-left"}>
                         <Separator className={"w-8 bg-primary mx-auto sm:mx-0"}/>
                         <span className={"text-sm text-center sm:text-left w-full text-balance"}> Terás acesso à nossa App exclusiva e ao teu plano de ação individualizado com o nosso acompanhamento diário para atingires os teus objetivos. </span>
                     </div>
 
-                    <div className={"flex flex-wrap gap-4 mt-4 justify-center sm:justify-start"}>
+                    <div className={"flex flex-wrap gap-4 justify-center sm:justify-start"}>
                         <Button variant={"outline"} className={"font-medium scroll-animate !duration-300"} asChild>
                             <a href={"/#planos"}>
                                 <NotebookPen className={"inline"} size={16}/>
