4c77f569fdbdf24d8816925f9e6c62181ec41d6c
ricardoneveslino <ricardoneveslino@gmail.com>
2025-03-21 18:58:05 +0000

    Mudança Video Apresentação

--- src/components/presentation-video.tsx
+++ src/components/presentation-video.tsx
@@ -15,7 +15,7 @@ const PresentationVideo = ( { className}: { className?: string} ) => {
                 containerElement={"div"}
                 aspectHeight={9}
                 aspectWidth={16}
-                id="eOlgI9k0oPc"
+                id="GVwghvbhMfo"
                 params="si=pBB7hSwp9Cu58iRd&rel=0&showinfo=0"
                 title="Vídeo de apresentação Team Lino"
             />
