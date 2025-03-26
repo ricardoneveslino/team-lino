72487f7c387596e1ad4c50e30184ce5329bd8c8a
rubenmlopes <rubenmiguel2725@icloud.com>
2025-03-26 21:32:45 +0000

    Meta Pixel e gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata } from "next"; 
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { montserrat } from "@/lib/fonts";
@@ -8,7 +8,8 @@ import { Footer } from "@/components/footer";
 import ScrollObserver from "@/components/scroll-observer";
 import ScrollLinked from "@/components/scroll-linked";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"; // Importando o SpeedInsights
+import { SpeedInsights } from "@vercel/speed-insights/next"; 
+import Script from "next/script"; // Import Script component for Pixel
 
 export const metadata: Metadata = {
   title: "Team Lino",
@@ -22,7 +23,64 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5C2FXDLZ');
+            `,
+          }}
+        />
+
+        {/* Meta Pixel Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1197155058406040');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
+
       <body className={`${montserrat.variable} antialiased`}>
+        {/* GTM (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5C2FXDLZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1197155058406040&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <ScrollLinked />
         <ScrollObserver />
         <ThemeProvider
@@ -35,7 +93,8 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </ThemeProvider>
-        <SpeedInsights /> {/* Adicionando o SpeedInsights aqui */}
+
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
