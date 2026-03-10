a1d0d3e5238064732bd1386c9534070be744662f
Ruben Miguel Lopes <rubenmiguellopes2725@gmail.com>
2026-03-10 11:06:01 +0000

    Novo  Update Imagens e planos

--- src/data/transformations.ts
+++ src/data/transformations.ts
@@ -1,43 +1,40 @@
 
 export const transformations = [
-    {
-        id: 1,
-        image: "/2.png"
-    },
-    {
-        id: 2,
-        image: "/3.png"
-    },
-    {
-        id: 3,
-        image: "/1.png"
-    },
-    {
-        id: 4,
-        image: "/4.png"
-    },
-    {
-        id: 5,
-        image: "/5.png"
-    },
-    {
-        id: 6,
-        image: "/6.png"
-    },
-    {
-        id: 7,
-        image: "/7.png"
-    },
-    {
-        id: 8,
-        image: "/8.png"
-    },
-    {
-        id: 9,
-        image: "/9.png"
-    },
-    {
-        id: 10,
-        image: "/10.png"
-    }
-]
+    { id: 1, image: "/2.png" },
+    { id: 2, image: "/3.png" },
+    { id: 3, image: "/1.png" },
+    { id: 4, image: "/4.png" },
+    { id: 5, image: "/5.png" },
+    { id: 6, image: "/6.png" },
+    { id: 7, image: "/7.png" },
+    { id: 8, image: "/8.png" },
+    { id: 9, image: "/9.png" },
+    { id: 10, image: "/10.png" },
+    { id: 11, image: "/11.png" },
+    { id: 12, image: "/12.png" },
+    { id: 13, image: "/13.png" },
+    { id: 14, image: "/14.png" },
+    { id: 15, image: "/15.png" },
+    { id: 16, image: "/16.png" },
+    { id: 17, image: "/17.png" },
+    { id: 18, image: "/18.png" },
+    { id: 19, image: "/19.png" },
+    { id: 20, image: "/20.png" },
+    { id: 21, image: "/21.png" },
+    { id: 22, image: "/22.png" },
+    { id: 23, image: "/23.png" },
+    { id: 24, image: "/24.png" },
+    { id: 25, image: "/25.png" },
+    { id: 26, image: "/26.png" },
+    { id: 27, image: "/27.png" },
+    { id: 28, image: "/28.png" },
+    { id: 29, image: "/29.png" },
+    { id: 30, image: "/30.png" },
+    { id: 31, image: "/31.png" },
+    { id: 32, image: "/32.png" },
+    { id: 33, image: "/33.png" },
+    { id: 34, image: "/34.png" },
+    { id: 35, image: "/35.png" },
+    { id: 36, image: "/36.png" },
+    { id: 37, image: "/37.png" },
+];
