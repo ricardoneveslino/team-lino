d83e79493437c0366f4ab86de4637809361e8f5f
Ruben Miguel Lopes <rubenmiguellopes2725@gmail.com>
2026-03-10 11:48:10 +0000

    mudar ordem

--- src/data/transformations.ts
+++ src/data/transformations.ts
@@ -1,8 +1,8 @@
 
 export const transformations = [
-    { id: 1, image: "/2.png" },
+    { id: 1, image: "/1.png" },
     { id: 2, image: "/3.png" },
-    { id: 3, image: "/1.png" },
+    { id: 3, image: "/2.png" },
     { id: 4, image: "/4.png" },
     { id: 5, image: "/5.png" },
     { id: 6, image: "/6.png" },
